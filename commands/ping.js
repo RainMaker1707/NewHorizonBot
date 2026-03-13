3df1aa25ff119c67ea463e568180b7a4264fbee6
RainMaker1707 <hadrienallegaert@icloud.com>
2026-03-13 20:00:31 +0100

    [ADD] bot.files.first_round
    configs.config: Will contains all the necessary global config for the bot.
    		It is intended to contains only the public configuration
    		has channels ID or title, hardcoded strings, etc.
    configs.secret: Is ignored from github but is intended to exist and contains all the private configuration (as API keys).
    commands.ping: Interface for the function that will handle the /ping command
    commands.close: Interface for the function that will handle the /close <ticket_id> command which will close the ticket channel.

---
+++ commands/ping.js
@@ -0,0 +1,12 @@
+const Discord = require('discord.js');
+
+
+module.exports = {
+  name: 'ping',
+  value: "N/A",
+  description: "Ping the bot to check if it is running or down",
+  options: [],
+  execute(interaction, client, callback) {
+      console.log("CheckID slashed")
+  }
+}
