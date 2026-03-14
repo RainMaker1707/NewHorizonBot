895448be10a707de60851d89c1b4b8d118b02e63
RainMaker1707 <hadrienallegaert@icloud.com>
2026-03-14 11:26:52 +0100

    [IMP] index.js: Add ping command and basic command logger in stdout

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 // Discord related import
 const DS = require('discord.js');
-const { Client, GatewayIntentBits, Partials} = require('discord.js');
-const { REST } = require('@discordjs/rest');
-const { Routes } = require('discord-api-types/v9');
+const { Client, GatewayIntentBits, Partials, REST, Routes } = require('discord.js');
 
 
 // External dependancies except discord related
@@ -15,18 +13,21 @@ const SECRET = require('./configs/secret.json');
 
 
 let bot = new Client({intents: [
-                                GatewayIntentBits.DirectMessages, 
-                                GatewayIntentBits.Guilds,
-		                        GatewayIntentBits.GuildMessages,
-                                GatewayIntentBits.MessageContent,
-                                GatewayIntentBits.GuildMembers,
-                                GatewayIntentBits.GuildMessageReactions,
-                                GatewayIntentBits.DirectMessageReactions,
-                                GatewayIntentBits.GuildVoiceStates,
-                                GatewayIntentBits.GuildPresences
-                            ],
-                      partials: [Partials.Message, Partials.Channel, Partials.Reaction]
-                    })
+            GatewayIntentBits.DirectMessages, 
+            GatewayIntentBits.Guilds,
+            GatewayIntentBits.GuildMessages,
+            GatewayIntentBits.MessageContent,
+            GatewayIntentBits.GuildMembers,
+            GatewayIntentBits.GuildMessageReactions,
+            GatewayIntentBits.DirectMessageReactions,
+            GatewayIntentBits.GuildVoiceStates,
+            GatewayIntentBits.GuildPresences
+        ],
+    partials: [Partials.Message, Partials.Channel, Partials.Reaction]
+})
+
+
+const rest = new REST({version: '10'}).setToken(SECRET.token);
 
 
 // Commands auto-loads from ./commands directory
@@ -37,8 +38,31 @@ for (const file of commandFiles) {
     commands.push(command);
 }
 
+(async () => {
+    try {
+        console.log('Refreshing application (/) commands.');
+        await rest.put(
+            Routes.applicationGuildCommands(CFG.clientId, CFG.guildId),
+            { body: commands }
+        );
+        console.log('Successfully reloaded application (/) commands.');
+    } catch (error) {
+        console.error(error);
+    }
+})();
 
-// Action the bot take just after login
+// Actions the bot take just after login
 bot.login(SECRET.token).then(async ()=> {
-    console.log(bot, "Bot is now live");
+    console.log(`Bot is now live: ${bot.user.tag}`);
+})
+
+// Actions the bot take on interaction (/) command
+bot.on("interactionCreate", async (interaction) => {
+    if(!interaction.isCommand()) return;
+    const command = interaction.commandName;
+    console.log(`Command received ${command} from ${interaction.user.displayName} id: ${interaction.user}`);
+    switch(command){
+        case "ping": try { interaction.reply("Pong! The bot is alive!"); } catch(err) {console.error(err)} break;
+        case _: console.log("Not implement command error! (Should never happens)")
+    }
 })
